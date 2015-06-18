1dc6f3a654c122b54079cdb69f0e8459afb2efc3
David Howells <dhowells@redhat.com>
2015-06-18 14:32:31 +0100

    overlayfs: Make f_path always point to the overlay and f_inode to the underlay
    [ Upstream commit 4bacc9c9234c7c8eec44f5ed4e960d9f96fa0f01 ]
    
    Make file->f_path always point to the overlay dentry so that the path in
    /proc/pid/fd is correct and to ensure that label-based LSMs have access to the
    overlay as well as the underlay (path-based LSMs probably don't need it).
    
    Using my union testsuite to set things up, before the patch I see:
    
    	[root@andromeda union-testsuite]# bash 5</mnt/a/foo107
    	[root@andromeda union-testsuite]# ls -l /proc/$$/fd/
    	...
    	lr-x------. 1 root root 64 Jun  5 14:38 5 -> /a/foo107
    	[root@andromeda union-testsuite]# stat /mnt/a/foo107
    	...
    	Device: 23h/35d Inode: 13381       Links: 1
    	...
    	[root@andromeda union-testsuite]# stat -L /proc/$$/fd/5
    	...
    	Device: 23h/35d Inode: 13381       Links: 1
    	...
    
    After the patch:
    
    	[root@andromeda union-testsuite]# bash 5</mnt/a/foo107
    	[root@andromeda union-testsuite]# ls -l /proc/$$/fd/
    	...
    	lr-x------. 1 root root 64 Jun  5 14:22 5 -> /mnt/a/foo107
    	[root@andromeda union-testsuite]# stat /mnt/a/foo107
    	...
    	Device: 23h/35d Inode: 40346       Links: 1
    	...
    	[root@andromeda union-testsuite]# stat -L /proc/$$/fd/5
    	...
    	Device: 23h/35d Inode: 40346       Links: 1
    	...
    
    Note the change in where /proc/$$/fd/5 points to in the ls command.  It was
    pointing to /a/foo107 (which doesn't exist) and now points to /mnt/a/foo107
    (which is correct).
    
    The inode accessed, however, is the lower layer.  The union layer is on device
    25h/37d and the upper layer on 24h/36d.
    
    Resolves: QTI9X28-797
    Change-Id: Ic3d85ffa8f305e45257679f97dc818ca8a3f1e70
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: rli <rli@sierrawireless.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1562,7 +1562,6 @@ struct inode_operations {
 	ssize_t (*get_qsize) (struct inode *);
 
 	/* WARNING: probably going away soon, do not use! */
-	int (*dentry_open)(struct dentry *, struct file *, const struct cred *);
 } ____cacheline_aligned;
 
 ssize_t rw_copy_check_uvector(int type, const struct iovec __user * uvector,
@@ -2084,7 +2083,6 @@ extern struct file *file_open_name(struct filename *, int, umode_t);
 extern struct file *filp_open(const char *, int, umode_t);
 extern struct file *file_open_root(struct dentry *, struct vfsmount *,
 				   const char *, int);
-extern int vfs_open(const struct path *, struct file *, const struct cred *);
 extern struct file * dentry_open(const struct path *, int, const struct cred *);
 extern int filp_close(struct file *, fl_owner_t id);
 
