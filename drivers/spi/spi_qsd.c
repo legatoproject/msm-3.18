6586b6a395d927fcf66e83b76da064c2d0246a56
Alyssa Lin <alin@sierrawireless.com>
2015-12-23 20:15:29 +0800

    NEW86544 - Support SPI 1-2 on DV1
    Issue Description:
    The default MTP SPI is in BLSP1 QUP1 and QUP2, but as customer's demand, we need to use QUP3 and QUP4 as the default spi port.
    
    Root Cause:
    N/A
    
    Solution:
        - Port spi driver from mdm9x15r55_core
        - Linux device tree add 2 spidev node, match gpios/clock on DV1
        - further improvement is addressed in DEV87174
    
    Change-Id: Ia55be739153ff0f6e0dd311633cfe721fab9ecaf
    Signed-off-by: Alyssa Lin <alin@sierrawireless.com>

--- drivers/spi/spi_qsd.c
+++ drivers/spi/spi_qsd.c
@@ -87,6 +87,12 @@ static inline void msm_spi_register_init(struct msm_spi *dd)
 	writel_relaxed(0x00000000, dd->base + SPI_OPERATIONAL);
 	writel_relaxed(0x00000000, dd->base + SPI_CONFIG);
 	writel_relaxed(0x00000000, dd->base + SPI_IO_MODES);
+/* SWISTART */
+/* SWI_TBD [YM:2015-12-23]: NEW86544, need to be improved later */
+#ifdef CONFIG_SIERRA
+	writel_relaxed(0x00000014, dd->base + SPI_DEASSERT_WAIT);
+#endif
+/* SWISTOP */
 	if (dd->qup_ver)
 		writel_relaxed(0x00000000, dd->base + QUP_OPERATIONAL_MASK);
 }
@@ -1774,6 +1780,50 @@ no_resources:
 err_setup_exit:
 	return rc;
 }
+
+/* SWISTART */
+#ifdef CONFIG_SIERRA
+void swi_reg_bit_set(struct spi_device *spi, int reg_addr, int bit_offset,int set_flag)
+{
+	u32 spi_ioc;
+	u32 spi_ioc_orig;
+	struct msm_spi	*dd;
+	dd = spi_master_get_devdata(spi->master);
+	clk_prepare_enable(dd->pclk);
+	spi_ioc = readl_relaxed(dd->base + reg_addr);
+	spi_ioc_orig = spi_ioc;
+	if (set_flag)
+		spi_ioc |= bit_offset;
+	else
+		spi_ioc &= ~bit_offset;
+
+	if (spi_ioc != spi_ioc_orig)
+		writel_relaxed(spi_ioc,dd->base + reg_addr);
+	clk_disable_unprepare(dd->pclk);
+}
+
+int swi_read_deassert_time(struct spi_device *spi)
+{
+	struct msm_spi	*dd;
+	int ret;
+	dd = spi_master_get_devdata(spi->master);
+	clk_prepare_enable(dd->pclk);
+	ret = readl_relaxed(dd->base + SPI_DEASSERT_WAIT);
+	clk_disable_unprepare(dd->pclk);
+	return ret;
+}
+
+void swi_write_deassert_time(struct spi_device *spi,u8 value)
+{
+	struct msm_spi	*dd;
+	dd = spi_master_get_devdata(spi->master);
+	clk_prepare_enable(dd->pclk);
+	writel_relaxed(value, dd->base + SPI_DEASSERT_WAIT);
+	clk_disable_unprepare(dd->pclk);
+}
+#endif
+/* SWISTOP */
+
 
 #ifdef CONFIG_DEBUG_FS
 
