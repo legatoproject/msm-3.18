bcbdeaa172c2078812d16c59be90855436e7aff9
Dragan Marinkovic <dmarinkovi@sierrawireless.com>
2016-02-22 11:27:58 -0800

    Add UBIFS quota support
    This rather large patch adds UBIFS file system quota support.
    It also fixes few UBIFS bugs. This patch requires kernel
    configurations changes as well:
    
    - Enable UBIFS quota support
    - Disable UBIFS ATIME support (even if support is there, in general
      atime will increase number of flash writes, and that is
      never a good idea). Use it if you really cannot live without it.
    - Enable old, v1 and v2 quota support in form of kernel modules.
    
    This patch is based on Dongsheng Yang's work, kernel version 4.2 .
    
    Lucy: this patch comes from in the following commit:
        commit 69e02a69860e8d77ce5a180c37c5f3f5d307afc1
        Author: Dragan Marinkovic <dmarinkovi@sierrawireless.com>
        Date:   Mon Feb 22 11:27:58 2016 -0800
        Add UBIFS quota support
    
    Resovles:QTI9X28-377
    Change-Id: I2d5b9580d4ec59863cc9852400452ea30b51766a
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
    (cherry picked from commit e527c5ee01f822a66e2678e60f176642fdb3eb90)
    
    Conflicts:
    
    	fs/ubifs/dir.c
    	fs/ubifs/file.c
    	fs/ubifs/super.c
    	fs/ubifs/ubifs.h
    
    Signed-off-by: Ryan Kirk <rkirk@sierrawireless.com>

--- fs/ubifs/ubifs.h
+++ fs/ubifs/ubifs.h
@@ -418,6 +418,9 @@ struct ubifs_inode {
 	loff_t synced_i_size;
 	loff_t ui_size;
 	int flags;
+#ifdef CONFIG_QUOTA
+	struct dquot *i_dquot[MAXQUOTAS];
+#endif
 	pgoff_t last_page_read;
 	pgoff_t read_in_a_row;
 	int data_len;
@@ -854,6 +857,8 @@ struct ubifs_compressor {
  *               have to be re-calculated
  * @new_page: non-zero if the operation adds a new page
  * @dirtied_page: non-zero if the operation makes a page dirty
+ * @new_block: non-zero if the operation adds a new block
+ * @dirtied_block: non-zero if the operation makes a block dirty
  * @new_dent: non-zero if the operation adds a new directory entry
  * @mod_dent: non-zero if the operation removes or modifies an existing
  *            directory entry
@@ -885,6 +890,8 @@ struct ubifs_budget_req {
 #ifndef UBIFS_DEBUG
 	unsigned int new_page:1;
 	unsigned int dirtied_page:1;
+	unsigned int new_block:1;
+	unsigned int dirtied_block:1;
 	unsigned int new_dent:1;
 	unsigned int mod_dent:1;
 	unsigned int new_ino:1;
@@ -895,6 +902,8 @@ struct ubifs_budget_req {
 	/* Not bit-fields to check for overflows */
 	unsigned int new_page;
 	unsigned int dirtied_page;
+	unsigned int new_block;
+	unsigned int dirtied_block;
 	unsigned int new_dent;
 	unsigned int mod_dent;
 	unsigned int new_ino;
@@ -983,6 +992,7 @@ struct ubifs_budg_info {
 	int page_budget;
 	int inode_budget;
 	int dent_budget;
+	int block_budget;
 };
 
 struct ubifs_debug_info;
@@ -1034,6 +1044,8 @@ struct ubifs_debug_info;
  * @bulk_read: enable bulk-reads
  * @default_compr: default compression algorithm (%UBIFS_COMPR_LZO, etc)
  * @rw_incompat: the media is not R/W compatible
+ * @usrquota: enable usrquota
+ * @grpquota: enable grpquota
  *
  * @tnc_mutex: protects the Tree Node Cache (TNC), @zroot, @cnext, @enext, and
  *             @calc_idx_sz
@@ -1275,6 +1287,8 @@ struct ubifs_info {
 	unsigned int bulk_read:1;
 	unsigned int default_compr:2;
 	unsigned int rw_incompat:1;
+	unsigned int usrquota:1;
+	unsigned int grpquota:1;
 
 	struct mutex tnc_mutex;
 	struct ubifs_zbranch zroot;
@@ -1743,11 +1757,14 @@ const struct ubifs_lprops *ubifs_fast_find_frdi_idx(struct ubifs_info *c);
 int ubifs_calc_dark(const struct ubifs_info *c, int spc);
 
 /* file.c */
+int ubifs_read_block(struct inode *inode, void *addr, unsigned int block,
+		      struct ubifs_data_node *dn);
 int ubifs_fsync(struct file *file, loff_t start, loff_t end, int datasync);
 int ubifs_setattr(struct dentry *dentry, struct iattr *attr);
 #ifdef CONFIG_UBIFS_ATIME_SUPPORT
 int ubifs_update_time(struct inode *inode, struct timespec *time, int flags);
 #endif
+ssize_t ubifs_get_qsize(struct inode *inode);
 
 /* dir.c */
 struct inode *ubifs_new_inode(struct ubifs_info *c, const struct inode *dir,
