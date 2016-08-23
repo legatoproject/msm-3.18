3278dd35e3fba24a05f7b73e4b13edbebc85e4df
Vincent Chen <vchen@sierrawireless.com>
2016-08-23 13:52:07 +0800

    [AR758x][AT] failed to enable HSIC via AT!CUSTOM
    Issue description:
    need at!custom="HSICENABLE",1/0 to enable/disable HSIC Ethernet function.
    
    Root cause:
    1,add at!custom="HSICENABLE",1/0 for customer
    2,if HSIC is enabled but not connected, it will consume about 80mA current, the workaround
    is disable HSIC host driver if customer don't use HSIC Ethernet chipset
    
    Solution:
    Add a new AT to control HSIC host driver is loaded or not loaded
    
    Change-Id: Ifdfccdc56f6581bcb565b6e7a6d2d34ffd6f32aa
    Resolves: QTI9X28-373

--- arch/arm/mach-msm/sierra_bscommon.c
+++ arch/arm/mach-msm/sierra_bscommon.c
@@ -74,3 +74,54 @@ uint64_t bsgetgpioflag(void)
 }
 EXPORT_SYMBOL(bsgetgpioflag);
 
+/************
+ *
+ * Name:     bsgethsicflag()
+ *
+ * Purpose:  Returns the hsic is enabled or not
+ *
+ * Parms:    none
+ *
+ * Return:   returns whether hsic is enabled or not
+ *
+ * Abort:    none
+ *
+ * Notes:
+ *
+ ************/
+bool bsgethsicflag(void)
+{
+	struct bccoworkmsg *mp;
+	unsigned char *virtual_addr;
+	uint32_t result = 0;
+
+	virtual_addr = sierra_smem_base_addr_get();
+	if (virtual_addr) {
+	 /*  APPL mailbox */
+	virtual_addr += BSMEM_COWORK_OFFSET;
+
+	mp = (struct bccoworkmsg *)virtual_addr;
+
+	if (mp->magic_beg == BS_SMEM_COWORK_MAGIC_BEG &&
+		mp->magic_end == BS_SMEM_COWORK_MAGIC_END ) {
+	 		/* doube check CRC */
+			if (mp->crc32 == crc32_le(~0, (void *)mp, BS_COWORK_CRC_SIZE)) {
+				/*get HSIC flag*/
+				result = (mp->bcfunctions)?1:0;
+			} else {
+				printk(KERN_ERR"sierra:-%s-failed: crc error", __func__);
+				return 0;
+			}
+    		} else {
+           		printk(KERN_ERR"sierra:-%s-failed: smem have not initized", __func__);
+           		return 0;
+		}
+	} else {
+		printk(KERN_ERR"sierra:-%s-failed: get virtual_add error", __func__);
+		return 0;
+	}
+
+	return result;
+}
+EXPORT_SYMBOL(bsgethsicflag);
+
