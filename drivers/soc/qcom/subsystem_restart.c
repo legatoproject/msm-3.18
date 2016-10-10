ee64b8cc5e9beed9189c86969e727b7ed8567e40
Yan Kong <ykong@sierrawireless.com>
2016-10-10 10:40:30 +0800

    Offer modem image loading interface to user space
    It is a sub task of "Boot-up time optimization".
    By default, modem image is not loaded until IPC data need to be sent.
    But modem image need to be loaded ASAP because most of features depend
    on it. As a result, Kernel need to offer a sysfs interface to user space,
    so that user space can trigger modem loading once modem image partition
    is mounted.
    
    Resolves: QTI9X40-784
    Change-Id: Iddae0621ad35ac949c29ed18524bb4b237baa93b
    Signed-off-by: Yan Kong <ykong@sierrawireless.com>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -308,6 +308,26 @@ static ssize_t system_debug_store(struct device *dev,
 	return orig_count;
 }
 
+static ssize_t firmware_load(struct device *dev,
+				struct device_attribute *attr, const char *buf,
+				size_t count)
+{
+	struct subsys_device *subsys = to_subsys(dev);
+	const char *p;
+	int orig_count = count;
+	int ret;
+
+	p = memchr(buf, '\n', count);
+	if (p)
+		count = p - buf;
+
+	if (!strncasecmp(buf, "1", count) &&
+		(subsystem_get(to_subsys(dev)->desc->fw_name) != NULL))
+		return 0;
+
+	return -EPERM;
+}
+
 int subsys_get_restart_level(struct subsys_device *dev)
 {
 	return dev->restart_level;
@@ -350,6 +370,7 @@ static struct device_attribute subsys_attrs[] = {
 	__ATTR(restart_level, 0644, restart_level_show, restart_level_store),
 	__ATTR(firmware_name, 0644, firmware_name_show, firmware_name_store),
 	__ATTR(system_debug, 0644, system_debug_show, system_debug_store),
+	__ATTR(firmware_load, 0644, NULL, firmware_load),
 	__ATTR_NULL,
 };
 
