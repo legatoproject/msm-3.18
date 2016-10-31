9b7e53f73e98b74fc91f0165eab26a3ea294abda
Brad Du <bdu@sierrawireless.com>
2016-10-31 14:11:42 -0700

    [BSP] add sierra ssmem driver
    Add Sierra SSMEM driver initial implementation.
    Same ssmem framework code will be also used in amss and LK.
    
    Testing:
    Tested using 'hexdump -s 0x30000 /dev/sierra_ssmem' and user space
    test app
    
    Resolves: QTI9X07-91
    Change-Id: I3b1bfdf6f8eaef97d2fe01e64bcd746985f87016
    Signed-off-by: Brad Du <bdu@sierrawireless.com>

---
+++ arch/arm/mach-msm/sierra/rules.mk
@@ -0,0 +1,16 @@
+# SWISTART
+# Sierra make file for LK
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+INCLUDES += -I$(LOCAL_DIR)/api
+
+CFLAGS += -DSWI_IMAGE_LK
+
+OBJS += \
+	$(LOCAL_DIR)/src/ssmem_lk.o \
+	$(LOCAL_DIR)/src/ssmem_core.o \
+	$(LOCAL_DIR)/src/ssmem_user.o \
+	$(LOCAL_DIR)/src/ssmem_utils.o
+
+# SWISTOP
