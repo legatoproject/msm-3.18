23da98f210b72e4a81427350b9515ee0a0b93435
ttao <ttao@sierrawireless.com>
2017-01-05 12:31:34 +0800

    customerX partitions flashing support in LK/SBL
    1, customerX partitions flashing support LK
    2, fastboot oem swi-get-ssdata
    3, fastboot oem swi-set-ssid
    4, Recovery_phase1 in LK before integrating Recovery_phase1 in SBL
    5, Resume bad_image flash when flashing partitions done
    
    Change-Id: I0d9eef1afa1ebd7f8730f0e237f6411306a0de41
    Resolves: QTI9X28-812
    Signed-off-by: ttao <ttao@sierrawireless.com>

--- arch/arm/mach-msm/include/mach/sierra_smem.h
+++ arch/arm/mach-msm/include/mach/sierra_smem.h
@@ -290,8 +290,9 @@
 #define DS_IMAGE_SYSTEM_2                   (1 << 16)  /* Root file system of system 2 */
 #define DS_IMAGE_USERDATA_1                 (1 << 17)  /* Legato FRM of system 1 */
 #define DS_IMAGE_USERDATA_2                 (1 << 18)  /* Legato FRM of system 2 */
-#define DS_IMAGE_CUSTOMER_0                 (1 << 19)  /* 'customer0' partition which stores customer application of system 1 */
-#define DS_IMAGE_CUSTOMER_2                 (1 << 20)  /* 'customer2' partition which stores customer application of system 2 */
+#define DS_IMAGE_CUSTOMERAPP_1              (1 << 19)  /* 'customer0' partition which stores customer application of system 1 */
+#define DS_IMAGE_CUSTOMERAPP_2              (1 << 20)  /* 'customer1' partition which stores customer application of system 2 */
+#define DS_IMAGE_CUSTOMERAPP_3              (1 << 21)  /* 'customer2' partition which stores customer application of system 2 */
 #define DS_IMAGE_FLAG_NOT_SET               0xFFFFFFFFFFFFFFFF /* Used during parameter delivery if 'bad image flag' not set */
 
 
