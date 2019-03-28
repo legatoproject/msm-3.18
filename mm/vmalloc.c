arter97/caf_msm-5.4

commit 0bf66754ebc04fb52c715e160184777570050e7d

mm: add preempt points into __purge_vmap_area_lazy()
Use cond_resched_lock to avoid holding the vmap_area_lock for a
potentially long time and thus creating bad latencies for various
workloads.

Change-Id: I36eb4d8dbd6604f52e5c463373a9754847a44bc2
[hch: split from a larger patch by Joel, wrote the crappy changelog]
Link: http://lkml.kernel.org/r/1479474236-4139-11-git-send-email-hch@lst.de
Signed-off-by: Joel Fernandes <joelaf@google.com>
Signed-off-by: Christoph Hellwig <hch@lst.de>
Tested-by: Jisheng Zhang <jszhang@marvell.com>
Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
Cc: Chris Wilson <chris@chris-wilson.co.uk>
Cc: John Dias <joaodias@google.com>
Cc: Thomas Gleixner <tglx@linutronix.de>
Cc: "H. Peter Anvin" <hpa@zytor.com>
Cc: Ingo Molnar <mingo@elte.hu>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Git-commit: 763b218ddfaf56761c19923beb7e16656f66ec62
Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
Signed-off-by: Sanjay Dwivedi <sanjaykd@codeaurora.org>

mm/vmalloc.c
@@ -651,7 +651,7 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 	LIST_HEAD(valist);
 	struct vmap_area *va;
 	struct vmap_area *n_va;
-	int nr = 0;
+	bool do_free = false;
 
 	/*
 	 * If sync is 0 but force_flush is 1, we'll go sync anyway but callers
@@ -674,7 +674,7 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 				*start = va->va_start;
 			if (va->va_end > *end)
 				*end = va->va_end;
-			nr += (va->va_end - va->va_start) >> PAGE_SHIFT;
+			do_free = true;
 			list_add_tail(&va->purge_list, &valist);
 			va->flags |= VM_LAZY_FREEING;
 			va->flags &= ~VM_LAZY_FREE;
@@ -682,16 +682,17 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 	}
 	rcu_read_unlock();
 
-	if (nr)
-		atomic_sub(nr, &vmap_lazy_nr);
-
-	if (nr || force_flush)
+	if (do_free || force_flush)
 		flush_tlb_kernel_range(*start, *end);
 
-	if (nr) {
+	if (do_free) {
 		spin_lock(&vmap_area_lock);
-		list_for_each_entry_safe(va, n_va, &valist, purge_list)
+		list_for_each_entry_safe(va, n_va, &valist, purge_list) {
+			int nr = (va->va_end - va->va_start) >> PAGE_SHIFT;
 			__free_vmap_area(va);
+			atomic_sub(nr, &vmap_lazy_nr);
+			cond_resched_lock(&vmap_area_lock);
+		}
 		spin_unlock(&vmap_area_lock);
 	}
 	spin_unlock(&purge_lock);

legatoproject/msm-3.18

commit 5c00842be70ff8703d55d0d970a372ee52571dde

mm: Revert "add preempt points into __purge_vmap_area_lazy()"
Revert commit 0bf66754ebc04fb52c715e160184777570050e7d which
caused the following exception:

BUG: sleeping function called from invalid context at /usr/src/kernel/mm/vmalloc.c:694
in_atomic(): 1, irqs_disabled(): 0, pid: 584, name: S09load_modem.s
Preemption disabled at:[<  (null)>]   (null)

CPU: 0 PID: 584 Comm: S09load_modem.s Not tainted 3.18.136 #1
[<c0014168>] (unwind_backtrace) from [<c0012870>] (show_stack+0x10/0x14)
[<c0012870>] (show_stack) from [<c00e832c>] (__purge_vmap_area_lazy+0x380/0x3ac)
[<c00e832c>] (__purge_vmap_area_lazy) from [<c00e84c8>] (free_vmap_area_noflush+0x68/0x74)
[<c00e84c8>] (free_vmap_area_noflush) from [<c00e9310>] (remove_vm_area+0x94/0x9c)
[<c00e9310>] (remove_vm_area) from [<c00e9370>] (__vunmap+0x58/0xc0)
[<c00e9370>] (__vunmap) from [<c02b2398>] (pil_boot+0xb1c/0xcb4)
[<c02b2398>] (pil_boot) from [<c02b662c>] (modem_powerup+0x98/0x108)
[<c02b662c>] (modem_powerup) from [<c02b8eec>] (__subsystem_get+0xe0/0x1dc)
[<c02b8eec>] (__subsystem_get) from [<c02b9040>] (firmware_load+0x50/0x68)
[<c02b9040>] (firmware_load) from [<c0162898>] (kernfs_fop_write+0x138/0x168)
[<c0162898>] (kernfs_fop_write) from [<c0103f60>] (vfs_write+0xb8/0x170)
[<c0103f60>] (vfs_write) from [<c0104394>] (SyS_write+0x3c/0x74)
[<c0104394>] (SyS_write) from [<c000e880>] (ret_fast_syscall+0x0/0x44)

Resolves: LXSWIREF-1295
Change-Id: Ib2ca1da83afac7dfae180df8fa2ec178b60a3642
Signed-off-by: Zoran Markovic <zmarkovic@sierrawireless.com>

mm/vmalloc.c
@@ -651,7 +651,7 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 	LIST_HEAD(valist);
 	struct vmap_area *va;
 	struct vmap_area *n_va;
-	bool do_free = false;
+	int nr = 0;
 
 	/*
 	 * If sync is 0 but force_flush is 1, we'll go sync anyway but callers
@@ -674,7 +674,7 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 				*start = va->va_start;
 			if (va->va_end > *end)
 				*end = va->va_end;
-			do_free = true;
+			nr += (va->va_end - va->va_start) >> PAGE_SHIFT;
 			list_add_tail(&va->purge_list, &valist);
 			va->flags |= VM_LAZY_FREEING;
 			va->flags &= ~VM_LAZY_FREE;
@@ -682,17 +682,16 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 	}
 	rcu_read_unlock();
 
-	if (do_free || force_flush)
+	if (nr)
+		atomic_sub(nr, &vmap_lazy_nr);
+
+	if (nr || force_flush)
 		flush_tlb_kernel_range(*start, *end);
 
-	if (do_free) {
+	if (nr) {
 		spin_lock(&vmap_area_lock);
-		list_for_each_entry_safe(va, n_va, &valist, purge_list) {
-			int nr = (va->va_end - va->va_start) >> PAGE_SHIFT;
+		list_for_each_entry_safe(va, n_va, &valist, purge_list)
 			__free_vmap_area(va);
-			atomic_sub(nr, &vmap_lazy_nr);
-			cond_resched_lock(&vmap_area_lock);
-		}
 		spin_unlock(&vmap_area_lock);
 	}
 	spin_unlock(&purge_lock);